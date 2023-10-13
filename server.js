c0421371293fa8d539241ce7104e62681c452ff1
vinay <vinaysoni.mca23@poddarinstitute.org>
2023-10-13 11:24:02 +0530

    updated frontend

--- server.js
+++ server.js
@@ -11,14 +11,25 @@ console.log("-----------view",path.join(__dirname, 'views'))
 app.set('views', path.join(__dirname, 'views'));
 
 const connectDatabase = require('./database.js')
-
+app.use(express.static(path.join(__dirname,'assets')));
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 // app.use(express.static(path.join(__dirname, 'views')));
 
 app.get('/', (req, res) => {
     // Render the EJS template, passing data as an object
-    res.render('index');
+    // res.render('index');
+    const jobs = [
+      { title: 'Job Title 1', description: 'Job Description 1' },
+      { title: 'Job Title 2', description: 'Job Description 2' },
+      { title: 'Job Title 1', description: 'Job Description 1' },
+      { title: 'Job Title 2', description: 'Job Description 2' },
+      { title: 'Job Title 1', description: 'Job Description 1' },
+      { title: 'Job Title 2', description: 'Job Description 2' },
+      // Add more job data as needed
+    ];
+
+    res.render('index', { jobs: jobs }); 
   });
 
 app.get('/about-us', (req, res) => {
