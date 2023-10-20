8bb30d6b3f2a7e815d65197ea41689b32610b5b5
vinaysoni-2001 <vinaysoni.mca@poddarinsitute.org>
2023-10-20 10:02:03 +0530

    updates in firebase and multer error to be fixed

--- controllers/Main.js
+++ controllers/Main.js
@@ -2,6 +2,21 @@ const path = require("path")
 const User = require("../models/User")
 const Jobs = require("../models/Jobs")
 const Contact = require("../models/Contact")
+// const {initializeApp} = require('firebase/app')
+// import { getStorage, ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage'
+// const {getStorage} = require("firebase/storage")
+// const {ref} = require("firebase/storage")
+// const {getDownloadURL} = require("firebase/storage")
+// const {uploadBytesResumable} = require("firebase/storage")
+// const multer = require("multer")
+// const config = require("../config/firebase")
+
+// initializeApp(config.firebaseConfig)
+// const storage = getStorage()
+// const upload = multer({storage:multer.memoryStorage()})
+
+
+
 exports.home = async (req, res) => {
  try {
     console.log("--------------req.body:",req.body)
@@ -30,12 +45,26 @@ exports.contact = async (req, res) => {
 
 
 //submitting data routes
+/*
 exports.createUser = async (req, res) => {
-  console.log("POST req made on" + req.url);
-  console.log("Form submitted to server");
-  const user = new User(req.body); //passing object of form data directly to collection
+  // console.log("POST req made on" + req.url);
+  // console.log("Form submitted to server");
+  // const user = new User(req.body); //passing object of form data directly to collection
   try{
+    console.log("--------req",req)
     console.log("----request body----create user",req.body);
+
+    process.exit(0);  
+    console.log("---------i am here");
+    const storageRef= ref(storage,req.file.originalname);console.log("---------i am storageref",storageRef);
+    const metadata={
+      contentType : req.file.mimetype,
+    }
+    console.log("---------i am metadata",req.file.mimetype);
+    const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+    const downloadURL = getDownloadURL(snapshot.ref);
+    console.log("---------i am downloadurl",downloadURL);
+
     const user = new User(req.body);
     await user.save();
     res.json({status:true,message:"success"});
@@ -46,6 +75,7 @@ exports.createUser = async (req, res) => {
 
   }
 }
+*/
 
 exports.createContact = async (req, res) => {
   console.log("POST req made on" + req.url);
--- server.js
+++ server.js
@@ -16,13 +16,62 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 // app.use(express.static(path.join(__dirname, 'views')));
 
-const {home,about,contact,createUser,createContact} = require("./controllers/Main")
+const {home,about,contact,createContact} = require("./controllers/Main")
+// will remove the code below
+const User = require("./models/User")
+const {initializeApp} = require('firebase/app')
+// import { getStorage, ref, getDownloadURL, uploadBytesResumable} from 'firebase/storage'
+const {getStorage} = require("firebase/storage")
+const {ref} = require("firebase/storage")
+const {getDownloadURL} = require("firebase/storage")
+const {uploadBytesResumable} = require("firebase/storage")
+const multer = require("multer")
+const config = require("./config/firebase")
 
+initializeApp(config.firebaseConfig)
+const storage = getStorage()
+const upload = multer({storage:multer.memoryStorage()})
+// will remove code till here
 app.get('/',home);
 
 app.get('/about-us', about);
 app.get('/contact',contact);
-app.post('/user',createUser);
+app.post('/user',upload.array("Resume"),async (req, res) => {
+    // console.log("POST req made on" + req.url);
+    // console.log("Form submitted to server");
+    // const user = new User(req.body); //passing object of form data directly to collection
+    try{
+      console.log("----request body----create user",req.body);
+      // const storageRef= ref(storage,req.file.originalname);console.log("---------i am storageref",storageRef);
+      // const metadata={
+      //   contentType : req.file.mimetype,
+      // }
+      // console.log("---------i am metadata",req.file.mimetype);
+      // const snapshot = await uploadBytesResumable(storageRef, req.file.buffer, metadata);
+      // const downloadURL = getDownloadURL(snapshot.ref);
+      // console.log("---------i am downloadurl",downloadURL);
+  
+      const userData={
+        "name":req.body.name,
+        "age":req.body.age,
+        "email":req.body.email,
+        "city":req.body.city,
+        "number":req.body.number
+      }
+      const user = new User(userData);//new User(req.body);
+      await user.save();
+      res.json({status:true,message:"success"});
+  
+    }catch(error){
+      console.log("-----error in creating user",error);
+      res.json({status:false, message:error.message});
+  
+    }
+  })
+
+
+
+
 app.post('/contact',createContact);
 
 
